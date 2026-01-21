b9fbdc0f6b166ecaeb10083295e85445f2d14134
tiarasugianto <2324343.tiara@smkn-2sbg.sch.id>
2026-01-21 14:07:00 +0700

    feat: define API routes for books

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookController;
+
+Route::get('/books', [BookController::class, 'index']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
