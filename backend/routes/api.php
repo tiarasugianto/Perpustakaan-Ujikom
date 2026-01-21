dbdf2464c11ab4f1ce39ba7d1e82c48b5051878b
tiarasugianto <2324343.tiara@smkn-2sbg.sch.id>
2026-01-21 13:41:34 +0700

    feat: define API login route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
