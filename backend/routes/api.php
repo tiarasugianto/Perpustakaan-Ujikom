9a20d19b208fe1331613514173a146683dbf663f
tiarasugianto <2324343.tiara@smkn-2sbg.sch.id>
2026-01-21 10:27:45 +0700

    refactor: connect api login route to auth controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
-
-Route::post('/login', function () {
-    return response()->json(['message' => 'API OK']);
-});
+Route::post('/login', [AuthController::class, 'login']);
