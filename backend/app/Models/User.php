1c8a22e3c37b52204025ed5cd41d80ea0c3c0829
tiarasugianto <2324343.tiara@smkn-2sbg.sch.id>
2026-01-21 13:30:39 +0700

    fix: clean up User model and define fillable attributes

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,33 +11,18 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
-        'role',
+        'role'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
