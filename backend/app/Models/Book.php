713575fbb57700b92f7356eded17398d06262265
tiarasugianto <2324343.tiara@smkn-2sbg.sch.id>
2026-01-21 14:01:55 +0700

    feat: add Book model

--- backend/app/Models/Book.php
+++ backend/app/Models/Book.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'judul',
+        'penulis',
+        'penerbit',
+        'tahun',
+        'stok'
+    ];
 }
