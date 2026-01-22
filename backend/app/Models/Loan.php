f683d17fe3d0c57b959bb05eb7e34b966336fe15
tiarasugianto <2324343.tiara@smkn-2sbg.sch.id>
2026-01-22 11:47:48 +0700

    feat: add Loan model

--- backend/app/Models/Loan.php
+++ backend/app/Models/Loan.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'tanggal_pinjam',
+        'tanggal_kembali',
+        'status'
+    ];
 }
