5318d792fec9daa25f03479134f1cd470083bcfb
tiarasugianto <2324343.tiara@smkn-2sbg.sch.id>
2026-01-22 13:20:13 +0700

    feat: define loan API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\LoanController;
+
+Route::get('/loans', [LoanController::class, 'index']);
+Route::post('/loans', [LoanController::class, 'store']);
 
 Route::get('/books', [BookController::class, 'index']);
 Route::post('/books', [BookController::class, 'store']);
