d3d201d07a5852585464ccfede5b86c4d2d675c8
tiarasugianto <2324343.tiara@smkn-2sbg.sch.id>
2026-03-31 09:48:01 +0700

    feat: menambahkan route API untuk login dan register user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,14 +5,16 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\LoanController;
 
+// ✅ loan routes
 Route::get('/loans', [LoanController::class, 'index']);
 Route::post('/loans', [LoanController::class, 'store']);
 
+// ✅ book routes
 Route::get('/books', [BookController::class, 'index']);
 Route::post('/books', [BookController::class, 'store']);
 Route::put('/books/{id}', [BookController::class, 'update']);
 Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
+// ✅ auth routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-
